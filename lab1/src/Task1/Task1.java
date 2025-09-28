91b061dc2ab19e0ed0fa8d48b7469bbd0ced3c4f
Maks Shymanskiy <maxshymanskiy@gmail.com>
2025-09-28 17:57:28 +0300

    (code): new convertToBinary

--- lab1/src/Task1/Task1.java
+++ lab1/src/Task1/Task1.java
@@ -2,21 +2,26 @@ package Task1;
 
 /*
 Завдання № 1.
-Перетворення заданого цілого числа у двійкову систему.
+    Перетворення заданого цілого числа у двійкову систему.
  */
-
-import java.util.Scanner;
 
 public class Task1 {
 
     public static void main(String[] args) {
-        final int number = 47;
+        final int number = 37;
         final String binaryNumber = convertToBinary(number);
         displayResult(number, binaryNumber);
     }
 
-    private static String convertToBinary(final int number) {
-        return Integer.toBinaryString(number);
+    private static String convertToBinary(int n) {
+        String s = "";
+
+        while (n > 0) {
+            s = ((n % 2 ) == 0 ? "0" : "1") + s;
+            n /= 2;
+        }
+
+        return s;
     }
 
     private static void displayResult(final int number,  final String result) {
