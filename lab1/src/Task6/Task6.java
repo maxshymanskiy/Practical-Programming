29f36be7dfee420e4576989e25b5926b5a565529
Maks Shymanskiy <maxshymanskiy@gmail.com>
2025-10-02 22:41:50 +0300

    (code): final Task6

--- lab1/src/Task6/Task6.java
+++ lab1/src/Task6/Task6.java
@@ -1,5 +1,12 @@
 package Task6;
 
+/*
+Завдання № 6.
+    Створити конвертер валют для: гривня, американський долар, канадський
+долар, євро. Введення даних у програму відбувається у вигляді “100 UAH
+into USD”, або в іншому зручному форматі.
+ */
+
 import java.util.Scanner;
 
 public class Task6 {
@@ -10,14 +17,14 @@ public class Task6 {
 
         try {
             final double result = converter.convert(input);
-            System.out.printf("Результат: %.2f%n", result);
+            System.out.printf("Result: %.2f%n", result);
         } catch (Exception e) {
-            System.out.println("Помилка: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage());
         }
     }
 
     private static String readInput() {
-        System.out.print("Введіть конвертацію (наприклад: 100 UAH into USD): ");
+        System.out.print("Enter conversion (for example: 100 UAH into USD): ");
         return new Scanner(System.in).nextLine();
     }
 }
